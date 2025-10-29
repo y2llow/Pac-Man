490c28e058869a70cccd8eb895fcbc159d454ffd
Eray Ayyildiz <eray.aartselaar@gmail.com>
2025-10-29 18:22:13 +0100

    julie was hier

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
-int main() {
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
 
     return 0;
 }
