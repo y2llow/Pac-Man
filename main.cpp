0b35271d953db00c0702ff6ca2c041ecfacd8efe
Eray Ayyildiz <eray.aartselaar@gmail.com>
2025-12-31 17:34:42 +0100

    added comments

--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@ int main(){
     game.run();
     return 0;
 }
-
+// todo add logic namespace
 // tijdens de 3 min video moet je niet de duidleijke dingen zeggen zeg de deingen die een probleem waren en hoe je die hebt gefixt
