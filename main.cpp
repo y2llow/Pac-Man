3619374236fcc1fe16dfd193ff8ae75a5f0c7c26
Eray Ayyildiz <eray.aartselaar@gmail.com>
2026-01-02 15:21:35 +0100

    minor fixes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include "representation/include/Game.h"
 
 int main(){
-    pacman::representation::Game game; // making an instance
+    pacman::representation::Game game;
     game.run();
     return 0;
 }
 
-// tijdens de 3 min video moet je niet de duidleijke dingen zeggen zeg de deingen die een probleem waren en hoe je die hebt gefixt
